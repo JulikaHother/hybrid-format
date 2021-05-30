b1abd03f13bc5fda5777570db15ec4eb4f5aa6fb
Jule <jule@F155-003-00000.fritz.box>
2021-05-30 16:31:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+let fragmente = document.querySelectorAll(".fragment")
+let curtain = document.querySelector("#curtain")
+let counter = 0;
+fragmente.forEach((elem) => {
+
+    elem.style.display = "none";
+    chnagePositionRandom(elem)
+
+})
+changeFragment()
+changeFragment()
+changeFragment()
+changeFragment()
+changeFragment()
+changeFragment()
+
+
+
+
+function changeFragment() {
+    let r = Math.floor(Math.random() * fragmente.length)
+    fragmente[r].style.display = "block"
+    chnagePositionRandom(fragmente[r])
+    fragmente.forEach((elem) => {
+
+        if (Math.floor(Math.random() * 5) == 1)
+            elem.style.display = "none";
+
+    })
+
+}
+
+
+
+document.addEventListener("wheel", (e) => {
+        if (counter % 2 == 0) {
+            changeFragment()
+        }
+        counter++
+
+    })
+    // document.addEventListener("click", (e) => {
+hideCurtain()
+    // })
+
+
+function chnagePositionRandom(elem) {
+    let elemWidth = elem.offsetWidth
+    let elemHeight = elem.offsetHeight
+    console.log(elemWidth);
+
+    let rx = Math.floor(Math.random() * (window.innerWidth - elemWidth))
+    let ry = Math.floor(Math.random() * (window.innerHeight - elemHeight))
+
+    elem.style.left = rx + "px";
+    elem.style.top = ry + "px";
+
+}
+
+function hideCurtain() {
+
+    let opacity = 1;
+    setTimeout(() => {
+        setInterval(() => {
+            curtain.style.opacity = opacity
+            opacity -= 0.1
+        }, 50);
+    }, 5000);
+}
